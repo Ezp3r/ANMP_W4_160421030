a3f7ca3f597d582670f7543579d25a805d1e244b
Ezper <S160421030@student.ubaya.ac.id>
2024-03-21 14:53:58 +0700

    homework done

--- app/src/main/java/com/ezper/advweek4160421030/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ezper/advweek4160421030/view/StudentDetailFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.ezper.advweek4160421030.R
 import com.ezper.advweek4160421030.databinding.FragmentStudentDetailBinding
 import com.ezper.advweek4160421030.databinding.FragmentStudentListBinding
+import com.ezper.advweek4160421030.viewmodel.DetailViewModel
 
 class StudentDetailFragment : Fragment() {
     private lateinit var binding: FragmentStudentDetailBinding
+    private lateinit var viewModel: DetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +28,22 @@ class StudentDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer { Student ->
+            binding.apply {
+                txtId.setText(Student.id)
+                txtName.setText(Student.name)
+                txtBod.setText(Student.dob)
+                txtPhone.setText(Student.phone)
+            }
+        })
     }
 
 }
